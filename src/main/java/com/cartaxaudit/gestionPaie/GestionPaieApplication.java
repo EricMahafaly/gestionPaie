a79e6d2a477a52501d7c39caaf94870d9d32a461
EricMahafaly <Ericmahafaly7@gmail.com>
2025-07-08 13:57:46 +0200

    init

---
+++ src/main/java/com/cartaxaudit/gestionPaie/GestionPaieApplication.java
@@ -0,0 +1,13 @@
+package com.cartaxaudit.gestionPaie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPaieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPaieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cartaxaudit/gestionPaie/GestionPaieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartaxaudit.gestionPaie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPaieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
