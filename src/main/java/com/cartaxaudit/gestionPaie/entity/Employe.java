76330c928022b6dd707ff13d0d261a06f413b088
EricMahafaly <Ericmahafaly7@gmail.com>
2025-07-09 10:22:06 +0200

    creation-employe

--- src/main/java/com/cartaxaudit/gestionPaie/entity/Employe.java
+++ src/main/java/com/cartaxaudit/gestionPaie/entity/Employe.java
@@ -1,8 +1,55 @@
 package com.cartaxaudit.gestionPaie.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
 
 
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
+@Table(name = "employe")
 public class Employe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idEmploye;
+
+    private String nom;
+
+    private String prenom;
+
+    private String adresse;
+
+    @Column(name = "numero_securite_sociale")
+    private String numeroSecuriteSociale;
+
+    private String email;
+
+    private String telephone;
+
+    private String poste;
+
+    private String departement;
+
+    @Column(name = "type_contrat")
+    private String typeContrat;
+
+    @Column(name = "date_embauche")
+    private LocalDate dateEmbauche;
+
+    @Column(name = "salaire_brut_mensuel")
+    private double salaireBrutMensuel;
+
+    @Column(name = "taux_horaire")
+    private double tauxHoraire;
+
+    @Column(name = "conges_cumules_par_mois")
+    private double congesCumulesParMois;
+
+    @Column(name = "solde_conge")
+    private double soldeConge;
 }
--- src/main/resources/base/script.sql
+++ src/main/resources/base/script.sql
@@ -1,2 +1,21 @@
 CREATE DATABASE gestion_paie;
 \c gestion_paie;
+
+
+CREATE TABLE employe (
+    id_employe SERIAL PRIMARY KEY,
+    nom VARCHAR(255),
+    prenom VARCHAR(255),
+    adresse VARCHAR(255),
+    numero_securite_sociale VARCHAR(255),
+    email VARCHAR(255),
+    telephone VARCHAR(255),
+    poste VARCHAR(255),
+    departement VARCHAR(255),
+    type_contrat VARCHAR(255),
+    date_embauche DATE,
+    salaire_brut_mensuel DOUBLE PRECISION,
+    taux_horaire DOUBLE PRECISION,
+    conges_cumules_par_mois DOUBLE PRECISION,
+    solde_conge DOUBLE PRECISION
+);
