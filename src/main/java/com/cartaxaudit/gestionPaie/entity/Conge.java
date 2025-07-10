69093663d9f15cd037812aa6860e998c940c5771
EricMahafaly <Ericmahafaly7@gmail.com>
2025-07-10 08:57:56 +0200

    creation entity

--- src/main/java/com/cartaxaudit/gestionPaie/entity/Conge.java
+++ src/main/java/com/cartaxaudit/gestionPaie/entity/Conge.java
@@ -1,12 +1,16 @@
 package com.cartaxaudit.gestionPaie.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "conge")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Conge {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/cartaxaudit/gestionPaie/service/CongeService.java
@@ -0,0 +1,11 @@
+package com.cartaxaudit.gestionPaie.service;
+
+import com.cartaxaudit.gestionPaie.repository.CongeRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CongeService {
+    private CongeRepository congeRepository;
+}
